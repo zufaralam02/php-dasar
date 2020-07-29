5151c098a59784b109448ccef4c37fd7663dcc65
Rofiif Zufar 'Allam <zufaralam02@gmail.com>
2020-07-30 01:34:25 +0700

    pertemuan 2

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -1,87 +1,55 @@
 <?php
 
-////// Sintaks PHP //////
+// Pertemuan 2 - PHP Dasar
+// Sintax PHP
 
-// 1. Standar Output
+// Standar Output
 // echo, print
 // print_r
 // var_dump
 
-// echo "Hello World";
-// print "Hello World";
-// print_r("Hello World");
-// var_dump("Hello World");
+// Penulisan Sintaks PHP
+// 1. PHP didalam HTML
+// 2. HTML didalam PHP
 
-// 2. Penulisan Sintaks PHP
-// - PHP di dalam HTML
-// - HTML di dalam PHP
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali angka, tapi boleh mengandung angka
+// $nama = "Rofiif Zufar Allam";
+// echo "Nama saya $nama";
 
-// 3. Variabel dan Tipe Data
-// variabel
-// - tidak boleh diawali dengan angka, tapi boleh mengandung angka
-// - tidak boleh ada spasi
-
-// $nama = "Zufar Allam";
-// echo 'halo nama saya $nama';
-// echo "halo nama saya $nama";
-
-// 4. Operator
-// - aritmatika
-// +, -, *, /, %
+// Operator
+// aritmatika
+// + - * / %
 // $x = 10;
 // $y = 20;
 // echo $x * $y;
 
-// - penggabung string / concatenation / concat
+// penggabung string / concatenation
 // .
 // $nama_depan = "Zufar";
 // $nama_belakang = "Allam";
-// echo $nama_depan . " " . $nama_belakang;
+// echo $nama_depan . " " .  $nama_belakang;
 
-// - assignment
+// assignment
 // =, +=, -=, *=, /=, %=, .=
 // $x = 1;
-// $x += 5;
+// $x -= 5;
 // echo $x;
 // $nama = "Zufar";
 // $nama .= " ";
 // $nama .= "Allam";
 // echo $nama;
 
-// - perbandingan
+// perbandingan
 // <, >, <=, >=, ==, !=
-// var_dump(1 < 5);
 // var_dump(1 == "1");
 
-// - identitas
-// ===, !==
+// identitas
+// ===, !===
 // var_dump(1 === "1");
 
-// - logika
-// $$, ||, !
-// $x = 10;
-// var_dump($x < 20 && $x % 2 == 0);
-// $x = 30;
-// var_dump($x < 20 && $x % 2 == 0);
-// var_dump($x < 20 || $x % 2 == 0);
-
-
-
-?>
-<!-- <!DOCTYPE html>
-<html>
-
-<head>
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Selamat Datang <?php echo $nama; ?></h1>
-    <p><?php echo "ini adalah paragraph"; ?></p>
-
-    <?php
-    echo "<h1>Selamat Datang kamu</h1>";
-    ?>
-</body>
-
-</html> -->
+// logika
+// &&, ||, !
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
